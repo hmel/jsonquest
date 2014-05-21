ae5f30b5efd6c3ebb0dae759fc314e23e0fc4066
Josh Harvey <josh@lamassu.is>
2014-05-21 00:27:03 -0400

    supports fingerprint checking

--- test/jsonquest-test.js
+++ test/jsonquest-test.js
@@ -1,114 +1,211 @@
-var http = require('http'),
-    assert = require('assert'),
-    cb = require('assert-called'),
-    jsonquest = require('../');
+'use strict';
+
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var assert = require('assert');
+var jsonquest = require('../');
 var querystring = require('querystring');
 
-var server = http.createServer(cb(function (req, res) {
-  var data = '';
-
-  assert.equal(req.method, 'PUT');
-  assert.equal(req.url, '/hello');
-  assert.equal(req.headers['content-type'], 'application/json');
-  assert.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz');
-
-  req.on('data', function (chunk) {
-    data += chunk.toString('utf8');
+describe('jsonquest', function() {
+  var server = null;
+  var port = 9995;
+  afterEach(function (done) {
+    server.close(done);
   });
 
-  req.on('end', function () {
-    assert.deepEqual(JSON.parse(data), { hello: 'world', witaj: 'świecie' });
+  it('should process json requests correctly', function (done) {    
+    server = http.createServer(function (req, res) {
+      var data = '';
 
-    res.writeHead(201, { 'content-type': 'application/json' });
-    res.write(JSON.stringify({ world: 'hello', świecie: 'witaj' }));
-    res.end();
-  });
-})).listen(9999, cb(function () {
-  jsonquest({
-    host: 'localhost',
-    port: 9999,
-    path: '/hello',
-    body: { hello: 'world', witaj: 'świecie' },
-    method: 'PUT',
-    protocol: 'http',
-    auth: 'user:pass'
-  }, cb(function (err, res, body) {
-    assert.equal(res.statusCode, 201);
-    assert.deepEqual(body, { world: 'hello', świecie: 'witaj' });
-    assert.equal(res.headers['content-type'], 'application/json');
+      assert.equal(req.method, 'PUT');
+      assert.equal(req.url, '/hello');
+      assert.equal(req.headers['content-type'], 'application/json');
+      assert.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz');
 
-    server.close();
-  }));
-}));
+      req.on('data', function (chunk) {
+        data += chunk.toString('utf8');
+      });
 
-var server3 = http.createServer(cb(function (req, res) {
-  var data = '';
+      req.on('end', function () {
+        assert.deepEqual(JSON.parse(data), { hello: 'world', witaj: 'świecie' });
 
-  assert.equal(req.method, 'PUT');
-  assert.equal(req.url, '/hello');
-  assert.equal(req.headers['content-type'], 'application/json');
-  assert.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz');
-
-  req.on('data', function (chunk) {
-    data += chunk.toString('utf8');
-  });
-
-  req.on('end', function () {
-    assert.deepEqual(JSON.parse(data), { hello: 'world', witaj: 'świecie' });
-
-    res.writeHead(201, { 'content-type': 'application/json' });
-    res.write('This is a string');
-    res.end();
-  });
-})).listen(9997, cb(function () {
-  jsonquest({
-    host: 'localhost',
-    port: 9997,
-    path: '/hello',
-    body: { hello: 'world', witaj: 'świecie' },
-    method: 'PUT',
-    protocol: 'http',
-    auth: 'user:pass'
-  }, cb(function (err, res, body) {
-    assert(err);
-    server3.close();
-  }));
-}));
-
-var server4 = http.createServer(cb(function (req, res) {
-  var data = '';
-
-  assert.equal(req.method, 'PUT');
-  assert.equal(req.url, '/hello');
-  assert.equal(req.headers['content-type'], 'application/x-www-form-urlencoded');
-  assert.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz');
-
-  req.on('data', function (chunk) {
-    data += chunk.toString('utf8');
+        res.writeHead(201, { 'content-type': 'application/json' });
+        res.write(JSON.stringify({ world: 'hello', świecie: 'witaj' }));
+        res.end();
+      });
+    }).listen(port, function () {
+      jsonquest({
+        host: 'localhost',
+        port: port,
+        path: '/hello',
+        body: { hello: 'world', witaj: 'świecie' },
+        method: 'PUT',
+        protocol: 'http',
+        auth: 'user:pass'
+      }, function (err, res, body) {
+        assert.equal(res.statusCode, 201);
+        assert.deepEqual(body, { world: 'hello', świecie: 'witaj' });
+        assert.equal(res.headers['content-type'], 'application/json');
+        done();
+      });
+    });
   });
 
-  req.on('end', function () {
-    assert.deepEqual(querystring.parse(data), { hello: 'world', witaj: 'świecie' });
+  it('should fail on bad auth', function (done) {
+    server = http.createServer(function (req, res) {
+      var data = '';
 
-    res.writeHead(201, { 'content-type': 'application/json' });
-    res.write(JSON.stringify({ world: 'hello', świecie: 'witaj' }));
-    res.end();
+      assert.equal(req.method, 'PUT');
+      assert.equal(req.url, '/hello');
+      assert.equal(req.headers['content-type'], 'application/json');
+      assert.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz');
+
+      req.on('data', function (chunk) {
+        data += chunk.toString('utf8');
+      });
+
+      req.on('end', function () {
+        assert.deepEqual(JSON.parse(data), { hello: 'world', witaj: 'świecie' });
+
+        res.writeHead(201, { 'content-type': 'application/json' });
+        res.write('This is a string');
+        res.end();
+      });
+    }).listen(port, function () {
+      jsonquest({
+        host: 'localhost',
+        port: port,
+        path: '/hello',
+        body: { hello: 'world', witaj: 'świecie' },
+        method: 'PUT',
+        protocol: 'http',
+        auth: 'user:pass'
+      }, function (err) {
+        assert(err);
+        done();
+      });
+    });
   });
-})).listen(9996, cb(function () {
-  jsonquest({
-    host: 'localhost',
-    port: 9996,
-    path: '/hello',
-    body: { hello: 'world', witaj: 'świecie' },
-    method: 'PUT',
-    protocol: 'http',
-    auth: 'user:pass',
-    requestEncoding: 'queryString'
-  }, cb(function (err, res, body) {
-    assert.equal(res.statusCode, 201);
-    assert.deepEqual(body, { world: 'hello', świecie: 'witaj' });
-    assert.equal(res.headers['content-type'], 'application/json');
-    server4.close();
-  }));
-}));
 
+  it('should handle queryString request', function (done) {
+    server = http.createServer(function (req, res) {
+      var data = '';
+
+      assert.equal(req.method, 'PUT');
+      assert.equal(req.url, '/hello');
+      assert.equal(req.headers['content-type'], 'application/x-www-form-urlencoded');
+      assert.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz');
+
+      req.on('data', function (chunk) {
+        data += chunk.toString('utf8');
+      });
+
+      req.on('end', function () {
+        assert.deepEqual(querystring.parse(data), { hello: 'world', witaj: 'świecie' });
+
+        res.writeHead(201, { 'content-type': 'application/json' });
+        res.write(JSON.stringify({ world: 'hello', świecie: 'witaj' }));
+        res.end();
+      });
+    }).listen(port, function () {
+      jsonquest({
+        host: 'localhost',
+        port: port,
+        path: '/hello',
+        body: { hello: 'world', witaj: 'świecie' },
+        method: 'PUT',
+        protocol: 'http',
+        auth: 'user:pass',
+        requestEncoding: 'queryString'
+      }, function (err, res, body) {
+        assert.equal(res.statusCode, 201);
+        assert.deepEqual(body, { world: 'hello', świecie: 'witaj' });
+        assert.equal(res.headers['content-type'], 'application/json');
+        done();
+      });
+    });
+  });
+
+  it('should fail on mismatched peer certificate', function (done) {
+    var httpsOptions = {
+      key: fs.readFileSync('test/fixtures/keys/key.pem'),
+      cert: fs.readFileSync('test/fixtures/keys/cert.pem')
+    };
+    server = https.createServer(httpsOptions, function (req, res) {
+      var data = '';
+
+      assert.equal(req.method, 'PUT');
+      assert.equal(req.url, '/hello');
+      assert.equal(req.headers['content-type'], 'application/json');
+
+      req.on('data', function (chunk) {
+        data += chunk.toString('utf8');
+      });
+
+      req.on('end', function () {
+        assert.equal(data, '');
+
+        res.writeHead(201, { 'content-type': 'application/json' });
+        res.write(JSON.stringify({ world: 'hello', świecie: 'witaj' }));
+        res.end();
+      });
+    }).listen(port, function () {
+      jsonquest({
+        host: 'localhost',
+        port: port,
+        path: '/hello',
+        body: { hello: 'world', witaj: 'świecie' },
+        method: 'PUT',
+        protocol: 'https',
+        fingerprint: 'xx'
+      }, function (err) {
+        assert.equal(err.message, 'Peer fingerprint doesn\'t match!');
+        done();
+      });
+    });
+  });
+
+  it('should succeed on correct peer certificate', function (done) {
+    var httpsOptions = {
+      key: fs.readFileSync('test/fixtures/keys/key.pem'),
+      cert: fs.readFileSync('test/fixtures/keys/cert.pem')
+    };
+    server = https.createServer(httpsOptions, function (req, res) {
+      var data = '';
+
+      assert.equal(req.method, 'PUT');
+      assert.equal(req.url, '/hello');
+      assert.equal(req.headers['content-type'], 'application/json');
+
+      req.on('data', function (chunk) {
+        data += chunk.toString('utf8');
+      });
+
+      req.on('end', function () {
+        assert.deepEqual(JSON.parse(data), { hello: 'world', witaj: 'świecie' });
+
+        res.writeHead(201, { 'content-type': 'application/json' });
+        res.write(JSON.stringify({ world: 'hello', świecie: 'witaj' }));
+        res.end();
+      });
+    }).listen(port, function () {
+      jsonquest({
+        host: 'localhost',
+        port: port,
+        path: '/hello',
+        body: { hello: 'world', witaj: 'świecie' },
+        method: 'PUT',
+        protocol: 'https',
+        fingerprint: '6B:32:68:91:B0:2D:01:10:59:DC:6F:2A:49:7C:13:7C:B6:05:68:EE'
+      }, function (err, res, body) {
+        assert.ifError(err);
+        assert.equal(res.statusCode, 201);
+        assert.deepEqual(body, { world: 'hello', świecie: 'witaj' });
+        assert.equal(res.headers['content-type'], 'application/json');
+        done();
+      });
+    });
+  });
+
+});
